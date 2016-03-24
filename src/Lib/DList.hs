c7102c49808f37856d2bcc9332ec6342bd63fe46
Dan Aloni <alonid@gmail.com>
2016-03-24 10:01:48 +0200

    Lib.DList: remove unused code

--- src/Lib/DList.hs
+++ src/Lib/DList.hs
@@ -1,9 +1,5 @@
 module Lib.DList
     ( dlistConcat
-    , unfoldrEDL
-    , unfoldrDL
-    , viewl
-    , ViewL(..)
     , dlistForM
     ) where
 
@@ -15,23 +11,6 @@ import qualified Data.DList                  as DList
 
 dlistConcat :: DList (DList a) -> DList a
 dlistConcat = DList.concat . DList.toList
-
-unfoldrEDL :: (b -> Either String (Maybe (b, DList a))) -> b -> (Either String (DList a), b)
-unfoldrEDL f = unfoldr' DList.empty
-    where unfoldr' as b = either (\s -> (Left s, b)) (z as b)  $ f b
-          z as b        = maybe (Right as, b)  $ \(b', a) -> unfoldr' (as `DList.append` a) b'
-
-unfoldrDL :: (b -> Maybe (b, DList a)) -> b -> (DList a, b)
-unfoldrDL f = unfoldr' DList.empty
-    where unfoldr' as b = maybe (as, b) (\(b', a) -> unfoldr' (as `DList.append` a) b') $ f b
-
-data ViewL a = EmptyL | a :<< DList a
-
-viewl :: DList a -> ViewL a
-viewl dl =
-    case DList.toList dl of
-         [] -> EmptyL
-         (a:xs) -> a :<< DList.fromList xs
 
 dlistMapM :: (Monad m, Foldable l) =>
            (a -> m b) -> l a -> m (DList b)
